db344cd44139ea3cf38bdf1e2edd0124a0555d4d
MatiasUrbina <matias.aleman.20@gmail.com>
2022-01-27 00:16:24 -0600

    Images resized, page optimization, greetings added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// GO TO TOP FUNCTIONS
 //Get the button
 let goToTopButton = document.getElementById("btn-back-to-top");
 
@@ -23,4 +24,53 @@ goToTopButton.addEventListener("click", backToTop);
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+// GRETTINGS FUNCTION
+// Using Function
+printGreetings("hour",false)
+
+// Function Declaration
+function printGreetings(id,replaceText) {
+  // The input is an ID from a label in HTML File
+  // Two possible text positions
+  //  - false: Puts the greetings in the begining of the text
+  //  - true: Replaces text with the greetings
+  // The output is the greetings in the ID labe
+
+  // Initialize Variables
+  let date = new Date();
+  let hour = date.getHours();
+  let hourString = "Hello!";
+  let actualText = document.getElementById(id);
+
+  // Select Case when a condition is TRUE
+  switch (true) {
+    // Good Morning between 05:00 am and before 12:00 pm
+    case (hour >= 5 && hour < 12):
+      hourString = "Good morning!"
+      break;
+
+    // Good Afternoon after 12:00 am and before 7:00 pm
+    case (hour >= 12 && hour < 18):
+      hourString = "Good afternoon!"
+      break;
+
+    // Good Night after 7:00 pm and before 05:00 am
+    case (hour >= 18 && hour <= 23 || hour >= 0 && hour < 5):
+      hourString = "Good evening!"
+      break;
+
+    default:
+      // Keep original text
+      break;
+  }
+  // Printing Greetings in ID
+  if (replaceText) {
+    // Replace Text
+    actualText.textContent = hourString;
+  } else {
+    // Add text at the begining
+    actualText.textContent = hourString+actualText.textContent;    
+  }
 }
